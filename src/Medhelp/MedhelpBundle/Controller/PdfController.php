d51613dd592a85a7e58b865538f51f016076ea0e
detmold <detmold12@gmail.com>
2015-09-27 21:03:06 +0200

    add bootstrap and pdf generator to diagnosis

--- src/Medhelp/MedhelpBundle/Controller/PdfController.php
+++ src/Medhelp/MedhelpBundle/Controller/PdfController.php
@@ -45,4 +45,61 @@ class PdfController extends Controller
 		return $response;
 
     }
+	
+	/**
+     * @Route("/diagnose/{id}")
+     */
+    public function diagnoseAction($id)
+    {
+		$em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MedhelpMedhelpBundle:Diagnosis')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Diagnosis entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+	
+		
+		$file = 'diagnose'.time().'.pdf';
+		$fileName = '/var/www/grojmy.pl/public_html/med/web/snappy/'.$file;
+		$this->get('knp_snappy.pdf')->generateFromHtml(
+			$this->renderView(
+				'MedhelpMedhelpBundle:Pdf:show.html.twig',
+				array(
+					'entity'      => $entity,
+					'delete_form' => $deleteForm->createView(),
+				)
+			),
+			$fileName
+		);
+		
+		$content = file_get_contents($fileName);
+		
+		$response = new Response();
+		$response->headers->set('Content-Type', 'application/pdf');
+		$response->headers->set('Content-Disposition', 'inline; filename='.$file);
+		$response->sendHeaders();
+		$response->setContent($content);
+		return $response;
+
+    }
+	
+	/**
+     * Creates a form to delete a Diagnosis entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('diagnosis_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
 }
